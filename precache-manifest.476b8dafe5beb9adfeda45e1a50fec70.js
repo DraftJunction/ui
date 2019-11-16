27e4e561208d918e544d4d890c32bae364ea0383
Vladimir Shifrin <vladimir_shifrin@apple.com>
2019-11-16 23:27:35 +0300

    Updates

---
+++ precache-manifest.476b8dafe5beb9adfeda45e1a50fec70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45233ba04ae0a2cd05b3e32c6ca6435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d2c490dfa0a0ecf2268",
+    "url": "/static/css/main.df600ad4.chunk.css"
+  },
+  {
+    "revision": "32c2d8c5d71eabf1321e",
+    "url": "/static/js/2.f238432c.chunk.js"
+  },
+  {
+    "revision": "0d2c490dfa0a0ecf2268",
+    "url": "/static/js/main.b318ec42.chunk.js"
+  },
+  {
+    "revision": "807549f3d4cff6fdaab2",
+    "url": "/static/js/runtime-main.ec384a63.js"
+  }
+]);
