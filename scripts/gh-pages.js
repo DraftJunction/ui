37d6715d5580bd979dddc991b9cc822c7762d56f
Vladimir Shifrin <vladimir_shifrin@apple.com>
2019-11-16 23:24:26 +0300

    Merge branch 'master' of https://github.com/logovazzik/draft
    # Conflicts:
    #	.idea/workspace.xml

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -2,7 +2,7 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('build', {
     branch: 'gh-pages',
-    repo: 'https://github.com/logovazzik/draft.git'
+    repo: 'https://github.com/DraftJunction/ui.git'
 }, (err) => {
     console.log(err);
 });
